7d1015d7824f333eafcc7d338ef559d972bd154c
atsnitya <varnika0311@gmail.com>
2025-07-13 21:40:52 +0530

    Added REST API to get country

--- Module7_SpringREST/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Module7_SpringREST/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,18 +1,32 @@
 package com.cognizant.springlearn.controller;
 
 import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
 
     @RequestMapping("/country")
     public Country getCountryIndia() {
         ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
         Country country = (Country) context.getBean("in");
         return country;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("START getCountry");
+        return countryService.getCountry(code);
     }
 }
