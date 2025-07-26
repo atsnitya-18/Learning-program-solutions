72f8f571f54afee5262235d82bbb6ba19ce01aa7
atsnitya <varnika0311@gmail.com>
2025-07-26 10:45:28 +0530

    uploded task 1 and 2 of react

---
+++ React/Studentapp react_2/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home.js';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Home />
+
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
